b12755ddd056566498061c53de0912cd5b592799
kclausen <kevinclausen@protonmail.com>
2023-10-08 22:08:33 +0200

    Added Description

--- StreamlitApp.py
+++ StreamlitApp.py
@@ -16,8 +16,6 @@ st.set_page_config(layout='wide')
 
 
 
-
-
 # Add a small offset to the Certainty values of the data points that have the same value
 offset = 0.07
 df_offset = df.groupby('Certainty', sort=False).apply(lambda x: x.assign(Certainty=x['Certainty'] + offset * x.groupby('Certainty').cumcount()))
@@ -31,18 +29,6 @@ subset_df = df_offset[['Indikator', 'Kategorie', 'STEEP-Kategorie', 'Certainty',
 subset_df = subset_df.dropna(subset=['Certainty', 'Impact'])
 
 
-# Define the color bands
-green_band = alt.Chart(pd.DataFrame({'x': [0, 3]})).mark_rect(color='green', opacity=0.2).encode(
-    x='x:Q'
-)
-
-yellow_band = alt.Chart(pd.DataFrame({'x': [3, 6]})).mark_rect(color='yellow', opacity=0.2).encode(
-    x='x:Q'
-)
-
-red_band = alt.Chart(pd.DataFrame({'x': [6, 9]})).mark_rect(color='red', opacity=0.2).encode(
-    x='x:Q'
-)
 
 point_groupOne = alt.Chart(subset_df.loc[subset_df['Prognose Group'] == 'Group 1']).mark_circle(size=60).encode(
     x=alt.X('Impact', scale=alt.Scale(zero=False), axis=x_axis),
@@ -100,7 +86,7 @@ scatter_plot3 = alt.Chart(subset_df.loc[subset_df['Prognose Group'] == 'Group 3'
 ).properties(
     width=500,
     height=600,
-    title=alt.TitleParams(text='10 - 15 Jahre', align='center', anchor='middle', fontSize=16)
+    title=alt.TitleParams(text='10 - 15+ Jahre', align='center', anchor='middle', fontSize=16)
 )
 
 # Link the zoom and movement of all three plots
@@ -112,13 +98,14 @@ scatter_plots = (scatter_plot1 + point_groupOne | scatter_plot2 + point_groupTwo
     x='shared',
     y='shared'
 )
-scatter_plots = alt.vconcat(scatter_plots, title=title)
+scatter_plots_with_title = alt.vconcat(scatter_plots, title=title)
 
 #with col2:
     #category = st.radio("Select a STEEP-Kategorie", ['Technological', 'Economical', 'Social', 'Political'])
 selection = alt.selection_single(fields=['STEEP-Kategorie'], name='Select')
 with col1:
-    st.altair_chart(scatter_plots, use_container_width=False)
+    st.markdown('## Früher-Prototyp zur Darstellung der Indikatoren und Mapping ohne Interaktion und erweiterte Funktionen')
+    st.altair_chart(scatter_plots_with_title, use_container_width=False)
 
 st.table(subset_df)
 
