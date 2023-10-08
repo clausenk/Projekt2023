aca69a97c995babb2654007be6b4d1e3310703c8
kclausen <kevinclausen@protonmail.com>
2023-10-08 20:01:31 +0200

    Prototype Fixes

--- StreamlitApp.py
+++ StreamlitApp.py
@@ -8,22 +8,11 @@ import altair as alt
 # Define the x and y axes
 x_axis = alt.Axis(title='Impact', grid=True)
 y_axis = alt.Axis(title=None, grid=True, labels=False)
+y_axis_with_labels = alt.Axis(title='Certainty', grid=True)
+
 # Load the Excel file
 df = pd.read_excel('indikatoren.xlsx')
 st.set_page_config(layout='wide')
-
-# Add a CSS style to change the background color to white
-st.markdown(
-    """
-    <style>
-    body {
-        background-color: #FFFFFF;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
 
 # Add a small offset to the Certainty values of the data points that have the same value
 offset = 0.07
@@ -52,12 +41,11 @@ red_band = alt.Chart(pd.DataFrame({'x': [6, 9]})).mark_rect(color='red', opacity
 
 point = alt.Chart(df).mark_circle(size=60).encode(
     x=alt.X('Impact', scale=alt.Scale(zero=False), axis=x_axis),
-    y=alt.Y('Certainty', scale=alt.Scale(zero=False), axis=y_axis),
+    y=alt.Y('Certainty', scale=alt.Scale(zero=False), axis=y_axis_with_labels),
     tooltip=['Indikator']
 ).properties(
-    width=600,
-    height=800,
-    title=alt.TitleParams(text='5 - 10 Jahre', align='center', anchor='middle', fontSize=16)
+    width=500,
+    height=600,
 ).interactive()
 
 # Create the scatter plot with padding
@@ -66,29 +54,29 @@ scatter_plot1 = alt.Chart(subset_df).mark_text(size=15, opacity=1.0, color='whit
     y=alt.Y('Certainty', title='Certainty', scale=alt.Scale(zero=False), axis=alt.Axis(format='~')),
     text='Indikator:N'
 ).properties(
-    width=600,
-    height=800,
+    width=500,
+    height=600,
     title=alt.TitleParams(text='0 - 5 Jahre', align='center', anchor='middle', fontSize=16)
 ).interactive()
 
 
-scatter_plot2 = alt.Chart(df).mark_circle(size=60).encode(
+scatter_plot2 = alt.Chart(subset_df).mark_text(size=15, opacity=1.0, color='white').encode(
     x=alt.X('Impact', scale=alt.Scale(zero=False), axis=x_axis),
     y=alt.Y('Certainty', scale=alt.Scale(zero=False), axis=y_axis),
-    tooltip=['Indikator']
+    text='Indikator:N'
 ).properties(
-    width=600,
-    height=800,
+    width=500,
+    height=600,
     title=alt.TitleParams(text='5 - 10 Jahre', align='center', anchor='middle', fontSize=16)
 ).interactive()
 
-scatter_plot3 = alt.Chart(df).mark_circle(size=60).encode(
+scatter_plot3 = alt.Chart(subset_df).mark_text(size=15, opacity=1.0, color='white').encode(
     x=alt.X('Impact', scale=alt.Scale(zero=False), axis=x_axis),
     y=alt.Y('Certainty', scale=alt.Scale(zero=False), axis=y_axis),
-    tooltip=['Indikator']
+    text='Indikator:N'
 ).properties(
-    width=600,
-    height=800,
+    width=500,
+    height=600,
     title=alt.TitleParams(text='10 - 15 Jahre', align='center', anchor='middle', fontSize=16)
 ).interactive()
 
@@ -97,14 +85,14 @@ scatter_plot3 = alt.Chart(df).mark_circle(size=60).encode(
 title = alt.TitleParams(text='Timeframe', align='center', anchor='middle', fontSize=20)
 
 
-scatter_plots = (scatter_plot1 + point | scatter_plot2 | scatter_plot3).resolve_scale(
+scatter_plots = (scatter_plot1 + point | scatter_plot2 + point | scatter_plot3 + point).resolve_scale(
     x='shared',
     y='shared'
 )
 scatter_plots = alt.vconcat(scatter_plots, title=title)
 
-with col2:
-    category = st.radio("Select a STEEP-Kategorie", ['Technological', 'Economical', 'Social', 'Political'])
+#with col2:
+    #category = st.radio("Select a STEEP-Kategorie", ['Technological', 'Economical', 'Social', 'Political'])
 selection = alt.selection_single(fields=['STEEP-Kategorie'], name='Select')
 with col1:
     st.altair_chart(scatter_plots, use_container_width=False)
